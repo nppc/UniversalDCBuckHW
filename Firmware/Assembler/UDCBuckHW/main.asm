bb97b69dfb1accd8dc57c8ce68c1441428f4d386
Pavel Palonen <pavel.palonen@gmail.com>
2018-09-02 21:28:16 +0200

    Cleanup and I reading error fix

--- Firmware/Assembler/UDCBuckHW/main.asm
+++ Firmware/Assembler/UDCBuckHW/main.asm
@@ -68,9 +68,7 @@ Voltage_Change:				.BYTE 1 ; (V*10). Before using this variable, we need to conv
 Voltage_Min:				.BYTE 1 ; (V*10)
 Voltage_Max:				.BYTE 1 ; (V*10)
 ; Variables (R)
-PWM_Value:					.BYTE 1
 Voltage_Measured:			.BYTE 1 ; (V*10)
-Current_Measured:			.BYTE 1 ; (A*10)
 ADC_Current_zero_RAW:		.BYTE 2	; ADC value when no load (0.0A)
 #ifdef MOVINGAVERAGE
 M_AVERAGE_voltage_COUNTER:	.BYTE 1	 ; Counter in the table
